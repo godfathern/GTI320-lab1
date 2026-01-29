2cfb39a225cb463e5c1ab6d9078481433144cf6a
Phan Tung Bui <phan-tung.bui.1@ens.etsmtl.ca>
2026-01-28 19:39:32 -0500

    test : unit tests addes for matrix

--- labo01/tests/TestsSupplementaires.cpp
+++ labo01/tests/TestsSupplementaires.cpp
@@ -12,6 +12,7 @@
 #include <gtest/gtest.h>
 
 #include "DenseStorage.h"
+#include  "Matrix.h"
 // Test operateur de setZero
 TEST(TestsSupplementaires, Supp01)
 {
@@ -33,14 +34,54 @@ TEST(TestsSupplementaires, Supp02)
     }
 }
 
+// Test operator= pour Matrix
 TEST(TestsSupplementaires, Supp03)
 {
+    gti320::Matrix<double, gti320::Dynamic, gti320::Dynamic, gti320::RowStorage> A(4, 3);
+    A.setZero();
 
+    for (int i = 0; i < A.rows(); ++i) {
+        for (int j = 0; j < A.cols(); ++j) {
+            A(i, j) = 100.0 * i + j;
+        }
+    }
+
+    gti320::Matrix<double, gti320::Dynamic, gti320::Dynamic, gti320::RowStorage> B(1, 1);
+    B = A;
+
+    EXPECT_EQ(B.rows(), 4);
+    EXPECT_EQ(B.cols(), 3);
+
+    for (int i = 0; i < B.rows(); ++i) {
+        for (int j = 0; j < B.cols(); ++j) {
+            EXPECT_DOUBLE_EQ(B(i, j), 100.0 * i + j);
+        }
+    }
+
+    // self-assignment
+    B = B;
+    for (int i = 0; i < B.rows(); ++i) {
+        for (int j = 0; j < B.cols(); ++j) {
+            EXPECT_DOUBLE_EQ(B(i, j), 100.0 * i + j);
+        }
+    }
 }
 
+// Test setIdentity pour Matrix (rectangulaire)
 TEST(TestsSupplementaires, Supp04)
 {
+    gti320::Matrix<double, gti320::Dynamic, gti320::Dynamic, gti320::ColumnStorage> M(3, 5);
+    M.setIdentity();
 
+    EXPECT_EQ(M.rows(), 3);
+    EXPECT_EQ(M.cols(), 5);
+
+    for (int i = 0; i < M.rows(); ++i) {
+        for (int j = 0; j < M.cols(); ++j) {
+            double expected = (i == j) ? 1.0 : 0.0;
+            EXPECT_DOUBLE_EQ(M(i, j), expected);
+        }
+    }
 }
 
 TEST(TestsSupplementaires, Supp05)
